5d2808f46952e9c277e6cddd060c396166a6a6f8
jingooo5 <jingooo5@g.skku.edu>
2024-11-02 22:57:24 +0900

    feat:separate login view

--- Router/item.py
+++ Router/item.py
@@ -54,28 +54,3 @@ def add_item(request : Request, user_id: str, item_id: str):
 @router.post("/{user_id}/receipt")
 def add_items(request : Request, user_id: str):
     return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item added successfully"})
-
-
-@router.post("/signup")
-def signup(request: Request, userCreate: UserCreate):
-    user = User(
-        name=userCreate.name,
-        password=userCreate.password
-    )
-    with get_db() as db:
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-
-    UserItemService.init_userItem(user.user_id)
-    return JSONResponse(status_code=HTTP_200_OK, content={"message": "User added successfully"})
-
-
-@router.post("/signin")
-def signin(request: Request, user: LoginUser):
-    with get_db() as db:
-        user = db.query(User).filter(User.name == user.name).first()
-        if user is None:
-            return JSONResponse(status_code=HTTP_204_NO_CONTENT, content={"message": "User not found"})
-
-        return JSONResponse(status_code=HTTP_200_OK, content={"message": "User signed in successfully", "user_id": user.user_id})
