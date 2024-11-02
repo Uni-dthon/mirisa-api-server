35de70785028dd1e5128bc402b7b6c4aadaaef32
jingooo5 <jingooo5@g.skku.edu>
2024-11-02 22:51:14 +0900

    feat:add login service

--- Router/item.py
+++ Router/item.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from fastapi.responses import JSONResponse
+
+from Data.user import LoginUser, UserCreate
 from Database.database import get_db
 from fastapi import APIRouter
 from starlette.status import *
@@ -53,3 +55,27 @@ def add_item(request : Request, user_id: str, item_id: str):
 def add_items(request : Request, user_id: str):
     return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item added successfully"})
 
+
+@router.post("/signup")
+def signup(request: Request, userCreate: UserCreate):
+    user = User(
+        name=userCreate.name,
+        password=userCreate.password
+    )
+    with get_db() as db:
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+    UserItemService.init_userItem(user.user_id)
+    return JSONResponse(status_code=HTTP_200_OK, content={"message": "User added successfully"})
+
+
+@router.post("/signin")
+def signin(request: Request, user: LoginUser):
+    with get_db() as db:
+        user = db.query(User).filter(User.name == user.name).first()
+        if user is None:
+            return JSONResponse(status_code=HTTP_204_NO_CONTENT, content={"message": "User not found"})
+
+        return JSONResponse(status_code=HTTP_200_OK, content={"message": "User signed in successfully", "user_id": user.user_id})
