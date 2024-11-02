54466d5a26baea0d22c38cf173743f736761e7e0
jingooo5 <jingooo5@g.skku.edu>
2024-11-02 20:17:24 +0900

    chore:fix some variables

--- Data/item.py
+++ Data/item.py
@@ -4,15 +4,16 @@ from datetime import date
 class Item(BaseModel):
     user_id : int
     item_name : str
-    count : int
+
 
 class ItemRead(Item):
-    consume_date : date
-
-class ItemCreate(Item):
-    pass
-
-
-class ItemUpdate(Item):
+    count : int
+    category : str
     consume_date: date
-    
+
+class ItemConsume(Item):
+    consume_count: int
+
+
+class ItemAdd(Item):
+    add_count: int
