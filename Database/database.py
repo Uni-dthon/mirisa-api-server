a263d5ae734efb15c83906507c62d17cf6d3347b
rocknroll17 <gon1397@naver.com>
2024-11-02 19:13:45 +0900

    feat:database and main.py added

---
+++ Database/database.py
@@ -0,0 +1,29 @@
+# database.py
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+load_dotenv(".env")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+TEMP_URL = "mysql+mysqlconnector://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_HOST + ":3306"
+DATABASE_URL = "mysql+mysqlconnector://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_HOST + ":3306/mirisa"
+temp_engine = create_engine(TEMP_URL)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+db = SessionLocal()
+
+def create_database():
+    with temp_engine.connect() as conn:
+        conn.execute(text(f"CREATE DATABASE IF NOT EXISTS planner"))
+        conn.commit()
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ main.py
@@ -0,0 +1,53 @@
+from contextlib import asynccontextmanager
+import sys
+import os
+import time
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from starlette.status import *
+import uvicorn
+from Database.database import db, create_database, engine
+from Router import *
+from dotenv import load_dotenv
+create_database()
+
+load_dotenv(".env")
+
+# 스웨거 예시 표시
+SWAGGER_HEADERS = {
+    "title": "SWAGGER API REFERENCE",
+    "version": "1.0.0",
+    "description": "",}
+
+# app = FastAPI()
+
+# 스웨거 예시 표시
+app = FastAPI(
+    swagger_ui_parameters={
+        "deepLinking": True,
+        "displayRequestDuration": True,
+        "docExpansion": "none",
+        "operationsSorter": "method",
+        "filter": True,
+        "tagsSorter": "alpha",
+        "syntaxHighlight.theme": "tomorrow-night",
+    },
+    **SWAGGER_HEADERS
+)
+
+# CORS
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=1500, reload=True)
