2ec4c3e9af7bcb6904b04239720dd8b0c174b92b
jingooo5 <jingooo5@g.skku.edu>
2024-11-02 22:07:38 +0900

    chore:add rough routers

--- Router/item.py
+++ Router/item.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi.responses import JSONResponse
 from Database.database import get_db
 from fastapi import APIRouter
@@ -10,6 +12,42 @@ router = APIRouter(tags=["items"], prefix="/items")
 @router.post("/add")
 def add_item(request: Request, item: Item):
     return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item added successfully"})
+
+
+"""
+유저가 보유한 모든 아이템을 가져온다.
+"""
+@router.get("/{user_id}/item", response_model=List[Item])
+def get_userItem_all(request : Request, user_id: str):
+    return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item added successfully"})
+
+
+"""
+유저가 아이템 소비
+TODO : request body에 아이템 개수 추가
+"""
+@router.post("/{user_id}/item/{item_id}/consume")
+def consume_item(request : Request, user_id: str, item_id: str):
+    return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item consumed successfully"})
+
+
+"""
+유저가 아이템 추가
+TODO : request body에 아이탬 개수 추가
+"""
+@router.post("/{user_id}/item/{item_id}/addition")
+def add_item(request : Request, user_id: str, item_id: str):
+    return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item added successfully"})
+
+
+"""
+유저가 영수증 / 직접 아이템 추가
+"""
+@router.post("/{user_id}/receipt")
+def add_items(request : Request, user_id: str):
+    return JSONResponse(status_code=HTTP_200_OK, content={"message": "Item added successfully"})
+
+
 
 @router.get("/test")
 def test():
