2b861fce6a2bc1e234556b2188da22089bcfdd8c
jingooo5 <jingooo5@g.skku.edu>
2024-11-03 03:46:24 +0900

    chore:update user_service create new user when user_id not exist

--- Router/login.py
+++ Router/login.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter
 from starlette.status import *
 from fastapi import Query, Request
 from Database.models import User
+from Service.user_service import UserService
 from Service.useritem_service import UserItemService
 from Utils.swagger import signin_response_example
 
@@ -17,17 +18,8 @@ router = APIRouter(tags=["login"], prefix="/login")
     500: {"description": "실패"}
 })
 def signup(request: Request, userCreate: UserCreate):
-    user = User(
-        name=userCreate.name,
-        password=userCreate.password
-    )
-    with get_db() as db:
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-
-    UserItemService.init_userItem(user.user_id)
-    return JSONResponse(status_code=HTTP_200_OK, content={"message": "User added successfully"})
+    user = UserService.get_user_or_create("", userCreate.name, userCreate.password)
+    return JSONResponse(status_code=HTTP_200_OK, content={"message": "User added successfully", "user_id": user.user_id})
 
 
 @router.post("/signin", summary="로그인", description="로그인 패스워드 확인안함", responses={
