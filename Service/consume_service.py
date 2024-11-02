c062fba115174aacfce6597a5c8f40eb8e65213e
rocknroll17 <gon1397@naver.com>
2024-11-03 03:21:05 +0900

    chore:fixed typo

--- Service/consume_service.py
+++ Service/consume_service.py
@@ -6,7 +6,6 @@ class ConsumeService:
     def consume_history_db(data : UserItemConsume):
         with get_db() as db:
             item_id = db.query(UserItem).filter(UserItem.user_id == data.user_id, UserItem.item_name == data.item_name).first().item_id
-            print(data.user_id, item_id)
             return ConsumeHistory(user_id=data.user_id, item_id=item_id, count=data.consume_count, date=data.consume_date)
         
     def purchase_history_save(data : ConsumeHistory):
